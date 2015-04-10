748c8bd938dc1dc05fb24b6c6342279599477448
Aaron <aaron@Mav3r1ck.com>
2015-04-10 05:50:36 -0400

    Added inline comments to the Questions.Swift file.

--- StudyApp/Questions.swift
+++ StudyApp/Questions.swift
@@ -14,6 +14,7 @@ struct Question {
   var answer: String
 }
 
+//List of Questions
 var firstQuestion = Question(askQuestion: "When was swift released?", answer: "June 2, 2014")
 var secondQuestion = Question(askQuestion: "Who Designed Swift?", answer: "Chris Lattner & many other programmers")
 var thirdQuestion = Question(askQuestion: "When was Swift 1.1 released?", answer: "October 22, 2014")
@@ -21,7 +22,11 @@ var forthQuestion = Question(askQuestion: "True or False: Swift and Objective-C 
 var fifthQuestion = Question(askQuestion: "Swift uses what to manage memory.", answer: "Automatic Reference Counting (ARC)")
 var sixthQuestion = Question(askQuestion: "Use ______ to make a constant", answer: "let")
 var seventhQuestion = Question(askQuestion: "Use ______ to make a variable", answer: "var")
+
+//Array of Questions
 var studyQuestions = [firstQuestion, secondQuestion, thirdQuestion, forthQuestion,fifthQuestion, sixthQuestion, seventhQuestion]
+
+//Setting the Index to -1 because on start we ++ the questionIndex making it zero.
 var questionIndex = -1
 
 
